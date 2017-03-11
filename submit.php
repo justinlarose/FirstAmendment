c4a813f8804ee054140afe3f088185dcb7d6201c
Justin LaRose <justin_larose@cable.comcast.com>
2017-03-10 23:52:23 -0500

    insert event into database

--- submit.php
+++ submit.php
@@ -27,7 +27,7 @@
 
                 <div class="cover-container">
 
-                    <div class="masthead clearfix">
+                    <div class="clearfix">
                         <div class="inner">
                             <h3 class="masthead-brand">1stAmendment</h3>
                             <nav>
@@ -42,94 +42,130 @@
                             <h1 class="cover-heading">Submit a New Protest</h1>
 
 
-                            <form>
+                            <form action="" method="post">
                                 <div class="form-group">
                                     <label for="inputEvent">Name of Event</label>
-                                    <input type="text" class="form-control" id="inputEvent" placeholder="Name of Event">
+                                    <input type="text" class="form-control" name="inputEvent" placeholder="Name of Event">
                                 </div>
 
                                 <div class="form-group">
                                     <label for="eventDescription">Description of Event</label>
-                                    <textarea class="form-control" rows="3" id="eventDescription" placeholder="Brief description of the event"></textarea>
+                                    <textarea class="form-control" rows="3" name="eventDescription" placeholder="Brief description of the event"></textarea>
                                 </div>
-
-                            </form>
-
-                            <!-- Text input-->
-                            <form class="form-inline">
 
                                 <div class="form-group">
-                                    <label for="inputDataTime1">Start Time</label>
-                                    <input type="datetime-local" class="form-control" id="inputDataTime1" >
+                                    <label for="inputStartDateTime">Start Time</label>
+                                    <input type="datetime-local" class="form-control" name="inputStartDateTime" >
                                 </div>
                                 <div class="form-group">
-                                    <label for="inputDataTime2">End Time</label>
-                                    <input type="datetime-local" class="form-control" id="inputDataTime2" >
+                                    <label for="inputEndDateTime">End Time</label>
+                                    <input type="datetime-local" class="form-control" name="inputEndDateTime" >
                                 </div>
-                            </form>
 
-                            <h4>Start Information</h4>
-                            <form>
+
+                                <h4>Start Information</h4>
+
                                 <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Street Address">
+                                    <input type="text" class="form-control" placeholder="Street Address" name="inputStartStreet">
                                 </div>
-                            </form>
-                            <form class="form-inline">
+
+
                                 <!-- Text input-->
                                 <div class="form-group">
 
-                                    <input type="text" class="form-control" placeholder="City">
+                                    <input type="text" class="form-control" placeholder="City" name="startCity">
                                 </div>
 
                                 <!-- Text input-->
                                 <div class="form-group">
 
-                                    <input type="text" class="form-control" placeholder="State" >
+                                    <input type="text" class="form-control" placeholder="State" name="startState">
                                 </div>
 
                                 <div class="form-group">
 
-                                    <input type="text" class="form-control" placeholder="Zipcode" >
+                                    <input type="text" class="form-control" placeholder="Zipcode" name="startZip" >
                                 </div>
 
-                            </form>
-
-                            <!-- Text input-->
-                            <h4>Destination Information</h4>
-                            <form>
 
 
-                                <div class="form-group">
-                                    <input type="text" class="form-control" placeholder="Street Address">
-                                </div>
-                            </form>
-                            <form class="form-inline">
                                 <!-- Text input-->
-                                <div class="form-group">
+                                <h4>Destination Information</h4>
 
-                                    <input type="text" class="form-control" placeholder="City">
+
+
+                                <div class="form-group">
+                                    <input type="text" class="form-control" placeholder="Street Address" name="inputEndStreet">
                                 </div>
+
 
                                 <!-- Text input-->
                                 <div class="form-group">
 
-                                    <input type="text" class="form-control" placeholder="State" >
+                                    <input type="text" class="form-control" placeholder="City" name="endCity">
+                                </div>
+
+                                <!-- Text input-->
+                                <div class="form-group">
+
+                                    <input type="text" class="form-control" placeholder="State" name="endState">
                                 </div>
 
                                 <div class="form-group">
 
-                                    <input type="text" class="form-control" placeholder="Zipcode" >
+                                    <input type="text" class="form-control" placeholder="Zipcode" name="endZip">
                                 </div>
 
-                            </form>
 
-
-
-
-                            <button type="submit" class="btn btn-default">Submit</button>
+                                <button type="submit" class="btn btn-default">Submit</button>
                             </form>
                         </div>
 
+                        <?php
+                        if ($_SERVER['REQUEST_METHOD'] == "POST") {
+                            $servername = "localhost";
+                            $username = "root";
+                            $password = "";
+                            $dbname = "1stamendment";
+
+                            $inputEvent = $_POST["inputEvent"];
+                            $eventDescription = $_POST["eventDescription"];
+                            $inputStartDateTime = $_POST["inputStartDateTime"];
+                            $inputEndDateTime = $_POST["inputEndDateTime"];
+                            $inputStartStreet = $_POST["inputStartStreet"];
+                            $startCity = $_POST["startCity"];
+                            $startState = $_POST["startState"];
+                            $startZip = $_POST["startZip"];
+                            $inputEndStreet = $_POST["inputEndStreet"];
+                            $endCity = $_POST["endCity"];
+                            $endState = $_POST["endState"];
+                            $endZip = $_POST["endZip"];
+
+
+                            try {
+                                $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+                                // set the PDO error mode to exception
+                                $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                                $sql = "INSERT INTO events (eventName, description, startTime, endTime, startAddress, startCity, startState, startZipCode, endAddress, endCity, endState, endZipCode) VALUES ('$inputEvent', '$eventDescription', '$inputStartDateTime', '$inputEndDateTime', '$inputStartStreet','$startCity','$startState','$startZip','$inputEndStreet','$endCity','$endState','$endZip')";
+                                // use exec() because no results are returned
+                                if ($conn->query($sql)) {
+                                    echo "<script type= 'text/javascript'>alert('New Record Inserted Successfully');</script>";
+                                } else {
+                                    echo "<script type= 'text/javascript'>alert('Data not successfully Inserted.');</script>";
+                                }
+                                //$conn->exec($sql);
+                                //echo "New record created successfully";
+                            } catch (PDOException $e) {
+                                echo $sql . "<br>" . $e->getMessage();
+                            }
+
+                            $conn = null;
+                        }
+                        ?>
+
+
+
+                        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
                         <script src="bootstrap/js/bootstrap.min.js"></script>
                         </body>
 
